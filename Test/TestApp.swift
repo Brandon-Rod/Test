86f384b9f7c666afab249bb8d946d94303803a8c
Brandon-Rod <rbrandon13@icloud.com>
2023-03-16 17:20:26 -0700

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Brandon Rodriguez on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
